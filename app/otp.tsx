aa14de56afd104c2eee2f3afa8af50b45ab73b9f
marcos-padilla <marcosalejandro.pd@gmail.com>
2024-04-16 11:06:46 -0400

    feat(frontend): added otp screen

---
+++ app/otp.tsx
@@ -0,0 +1,255 @@
+import Colors from '@/constants/Colors'
+import { Ionicons } from '@expo/vector-icons'
+import { useRouter } from 'expo-router'
+import { useState } from 'react'
+import {
+	View,
+	Text,
+	StyleSheet,
+	Linking,
+	TouchableOpacity,
+	KeyboardAvoidingView,
+	Platform,
+	ActivityIndicator,
+	Alert,
+} from 'react-native'
+import MaskInput from 'react-native-mask-input'
+import {
+	isClerkAPIResponseError,
+	useSignIn,
+	useSignUp,
+} from '@clerk/clerk-expo'
+
+const US_PHONE = [
+	`+`,
+	'1',
+	' ',
+	'(',
+	/\d/,
+	/\d/,
+	/\d/,
+	')',
+	' ',
+	/\d/,
+	/\d/,
+	/\d/,
+	'-',
+	/\d/,
+	/\d/,
+	/\d/,
+	/\d/,
+]
+
+const Page = () => {
+	const [phoneNumber, setPhoneNumber] = useState('')
+	const keyboardVerticalOffset = Platform.OS === 'ios' ? 90 : 0
+	const router = useRouter()
+	const [loading, setLoading] = useState(false)
+	const { signUp, setActive } = useSignUp()
+	const { signIn } = useSignIn()
+
+	const sendOTP = async () => {
+		console.log('sendOTP', phoneNumber)
+		setLoading(true)
+
+		try {
+			await signUp!.create({
+				phoneNumber,
+			})
+
+			signUp!.preparePhoneNumberVerification()
+
+			router.push(`/verify/${phoneNumber}`)
+		} catch (err) {
+			if (isClerkAPIResponseError(err)) {
+				if (err.errors[0].code === 'form_identifier_exists') {
+					// User signed up before
+					console.log('User signed up before')
+					await trySignIn()
+				} else {
+					setLoading(false)
+					Alert.alert('Error', err.errors[0].message)
+				}
+			}
+		}
+	}
+
+	const trySignIn = async () => {
+		const { supportedFirstFactors } = await signIn!.create({
+			identifier: phoneNumber,
+		})
+
+		const firstPhoneFactor: any = supportedFirstFactors.find(
+			(factor: any) => {
+				return factor.strategy === 'phone_code'
+			}
+		)
+
+		const { phoneNumberId } = firstPhoneFactor
+
+		await signIn!.prepareFirstFactor({
+			strategy: 'phone_code',
+			phoneNumberId,
+		})
+
+		router.push(`/verify/${phoneNumber}?signin=true`)
+		setLoading(false)
+	}
+
+	return (
+		<KeyboardAvoidingView
+			keyboardVerticalOffset={keyboardVerticalOffset}
+			style={{ flex: 1 }}
+			behavior='padding'
+		>
+			{loading && (
+				<View style={[StyleSheet.absoluteFill, styles.loading]}>
+					<ActivityIndicator
+						size='large'
+						color={Colors.primary}
+					/>
+					<Text style={{ fontSize: 18, padding: 10 }}>
+						Sending code...
+					</Text>
+				</View>
+			)}
+
+			<View style={styles.container}>
+				<Text style={styles.description}>
+					WhatsApp will need to verify your account. Carrier
+					charges may apply.
+				</Text>
+
+				<View style={styles.list}>
+					<View style={styles.listItem}>
+						<Text style={styles.listItemText}>
+							United States
+						</Text>
+						<Ionicons
+							name='chevron-forward'
+							size={20}
+							color={Colors.gray}
+						/>
+					</View>
+					<View style={styles.separator} />
+
+					<MaskInput
+						value={phoneNumber}
+						keyboardType='numeric'
+						autoFocus
+						placeholder='+1 your phone number'
+						onChangeText={(masked, unmasked) => {
+							setPhoneNumber(masked)
+						}}
+						mask={US_PHONE}
+						style={styles.input}
+					/>
+				</View>
+
+				<Text style={styles.legal}>
+					You must be{' '}
+					<Text style={styles.link}>at least 16 years old</Text>{' '}
+					to register. Learn how WhatsApp works with the{' '}
+					<Text style={styles.link}>Meta Companies</Text>.
+				</Text>
+
+				<View style={{ flex: 1 }} />
+
+				<TouchableOpacity
+					style={[
+						styles.button,
+						phoneNumber !== '' ? styles.enabled : null,
+						{ marginBottom: 20 },
+					]}
+					onPress={sendOTP}
+				>
+					<Text
+						style={[
+							styles.buttonText,
+							phoneNumber !== '' ? styles.enabled : null,
+						]}
+					>
+						Next
+					</Text>
+				</TouchableOpacity>
+			</View>
+		</KeyboardAvoidingView>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		padding: 20,
+		backgroundColor: Colors.background,
+		gap: 20,
+	},
+	description: {
+		fontSize: 14,
+		color: Colors.gray,
+	},
+	legal: {
+		fontSize: 12,
+		textAlign: 'center',
+		color: '#000',
+	},
+	link: {
+		color: Colors.primary,
+	},
+	button: {
+		width: '100%',
+		alignItems: 'center',
+		backgroundColor: Colors.lightGray,
+		padding: 10,
+		borderRadius: 10,
+	},
+	enabled: {
+		backgroundColor: Colors.primary,
+		color: '#fff',
+	},
+	buttonText: {
+		color: Colors.gray,
+		fontSize: 22,
+		fontWeight: '500',
+	},
+	list: {
+		backgroundColor: '#fff',
+		width: '100%',
+		borderRadius: 10,
+		padding: 10,
+	},
+	listItem: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		padding: 6,
+		marginBottom: 10,
+	},
+	listItemText: {
+		fontSize: 18,
+		color: Colors.primary,
+	},
+	separator: {
+		width: '100%',
+		height: 1,
+		backgroundColor: Colors.gray,
+		opacity: 0.2,
+	},
+	input: {
+		backgroundColor: '#fff',
+		width: '100%',
+		fontSize: 16,
+		padding: 6,
+		marginTop: 10,
+	},
+
+	loading: {
+		zIndex: 10,
+		backgroundColor: '#fff',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+})
+
+export default Page
